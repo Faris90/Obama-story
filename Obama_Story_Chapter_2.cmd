4175442557886a13c5e8e535e9c5169d9a7e6ed1
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2020-01-26 19:04:10 -0600

    Add files via upload

---
+++ Obama_Story_Chapter_2.cmd
@@ -0,0 +1,16 @@
+@echo off
+title OBAMA STORY CHAPTER 2: THE BOOF [By Yes'd]
+cls
+:start
+echo OBAMA passes you the BOOF.
+set choice=
+set /p choice=Do you accept OBAMA's gift? [y/n] : 
+
+if '%choice%'=='familyguyispoop' echo You have been teleported to a dark realm. You see OBAMA approaching you. HE says something in an alien language and you are turned into dust by a mysterious force. [GAME OVER - ENDING:HORRIBLE]
+pause
+
+if '%choice%'=='y' echo You accept OBAMA's BOOF, You are now an ascended being for that was a Legendary Boof. [GAME OVER - ENDING:GOOD]
+pause
+
+if '%choice%'=='n' echo You refuse OBAMA's BOOF, OBAMA smokes the BOOF by himself, you do not get the BOOF. [GAME OVER - ENDING:SAD]
+pause
